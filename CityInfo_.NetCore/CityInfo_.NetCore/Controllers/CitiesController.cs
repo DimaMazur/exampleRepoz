bc07057d7a08c7e598af0c5d7de1bff8dd6d0577
Dmytro Mazur <d.mazur@levi9.com>
2017-10-08 23:45:33 +0300

    Added first API controller

---
+++ CityInfo_.NetCore/CityInfo_.NetCore/Controllers/CitiesController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo_.NetCore.Controllers
+{
+    [Route("api/[controller]")]
+    public class CitiesController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(new List<object>
+            {
+                new { id = 1, Name = "New York city" },
+                new { id = 2, Name = "Antwerpen" }
+            });
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
